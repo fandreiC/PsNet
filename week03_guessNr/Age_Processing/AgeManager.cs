a493212a59d6307d5433f1890c81f929600dbf83
andrei kattsw <andrei_kattsw@yahoo.com>
2019-12-09 23:02:30 +0200

    Age & strings

---
+++ week03_guessNr/Age_Processing/AgeManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace Age_Processing
+{
+    class Program
+    {
+        private enum genderTypes { M, F }
+        private static int month, year, day;
+        private static genderTypes? gender;
+        private static DateTime birthDay;
+        static void Main(string[] args)
+        {
+            ReadDataForAge();
+            bool is_validDate = TransformDataForAge();
+            int age;
+            if (is_validDate)
+            {
+                age = CalculateAge();
+
+                if (gender != null)
+                {
+                    if (gender == genderTypes.M)
+                        if (age > 65)
+                            Console.WriteLine("You are retired! (Male)");
+                        else Console.WriteLine($"you need {65-age} more years to retire");
+                    else
+                        if (age > 63)
+                        Console.WriteLine("You are retired! (Female)");
+                        else Console.WriteLine($"you need {63 - age} more years to retire");
+                }
+            }
+            Console.ReadKey();
+        }
+        private static int CalculateAge()
+        {
+            int age = DateTime.Today.Year - birthDay.Year;
+            Console.WriteLine("Your age is : " + age);
+            return age;
+        }
+        private static bool TransformDataForAge()
+        {
+            string birthdayValue = year + "-" + month + "-" + day;
+            if (!DateTime.TryParse(birthdayValue, out birthDay))
+            {
+                Console.WriteLine("error reading date ! Program will end ");
+                return false;
+            }
+            return true;
+        }
+
+        private static void ReadDataForAge()
+        {
+            Console.WriteLine(" Insert your birthday, please!");
+
+            string genderValue;
+            bool checkValues;
+            do
+            {
+                checkValues = true;
+                Console.WriteLine("Insert year >> ");
+                Int32.TryParse(Console.ReadLine(), out year);
+                Console.WriteLine("Insert month >> ");
+                Int32.TryParse(Console.ReadLine(), out month);
+                Console.WriteLine("Insert day >> ");
+                Int32.TryParse(Console.ReadLine(), out day);
+                Console.WriteLine("Insert Gender (M-Male/F-Female) : (M/F) >> ");
+                //Enum.TryParse<genderTypes>( Console.ReadLine(),out gender);
+                genderValue = Console.ReadLine().ToUpper();
+                if (year < 1000 || year > DateTime.Today.Year || month < 1 || month > 12 || day < 1 || day > 31)
+                {
+                    checkValues = false;
+                    Console.WriteLine("You inserted invalid data! Retry:");
+                }
+                bool genderCheck = Enum.GetNames(typeof(genderTypes)).Contains(genderValue);
+                if (!genderCheck)
+                    gender = null;
+                else
+                    gender = genderValue == genderTypes.M.ToString() ? genderTypes.M : genderTypes.F; 
+
+
+            } while (!checkValues);
+        }
+    }
+}
---
+++ week03_guessNr/StringPlay/StringPlayManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace StringPlay
+{
+    class StringPlayManager
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Strings!");
+
+            string longText = "    @This @is @a @phrase @with @spaces!    ";
+            longText= longText.Trim();
+            string[] words = longText.Split(' ');
+            string[] wordsNew = new string[words.Length];
+            int x = 0;
+            foreach (var word in words)
+            {                
+                if (word.Contains("@"))
+                   wordsNew[x++]=word.Remove(0,1);
+            }
+            string space = "spaces!";
+            string peace;
+            if (wordsNew[5].ToLower().Equals(space.ToLower()))
+                peace=space.Replace("s","");
+            if (longText.StartsWith("@"))
+                longText=longText.Replace("@", "#");
+            string url = "c:\\Local\\User\\file.cs";
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10; i++)
+            {
+                sb.Append(" New year 200" + i);
+                if (i == 4)
+                    sb.AppendLine();
+            }
+            sb.Replace("00","99");
+
+
+            Console.ReadKey();
+        }
+    }
+}
