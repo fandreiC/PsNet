cb12c198da796cca42d7c5e244ba6cce3ac4d339
andrei kattsw <andrei_kattsw@yahoo.com>
2020-02-19 23:51:46 +0200

    asp small update

---
+++ Asp_week01/MVCPentaStagiu01/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using MVCPentaStagiu01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPentaStagiu01.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return RedirectToAction("Details");
+        }
+
+        [HttpGet]
+        public ActionResult Details()
+        {
+            UserViewModel userViewModel = new UserViewModel
+            {
+                Age = 17,
+                FirstName = "Hulk"
+            };
+            return View(userViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Description = "This is a create page !";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserViewModel userViewModel )
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return RedirectToAction("Index");
+            
+        }
+
+    }
+}
---
+++ Asp_week01/MVCPentaStagiu01/Models/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCPentaStagiu01.Models
+{
+    public class UserViewModel
+    {
+        [Required]
+        [Display(Name = "Nume" )]
+        public string  FirstName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
